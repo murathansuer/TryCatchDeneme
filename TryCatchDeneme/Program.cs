a3d3c7f29f130c5c9c73689ae5a15f00e5e12227
Murathan Süer <murathan.suer@gmail.com>
2023-01-31 16:44:19 +0300

    Proje dosyası ekle.

---
+++ TryCatchDeneme/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TryCatchDeneme
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+
+                    //// KENDİME NOT!!
+                    ///////// SWITCH CASE BREAK YAPISINI ACIL OGREN, 
+                    ///DAHA HIZLI İLERLEME İÇİN
+                    ///BÜTÜN KODLARI IF İLE YAZMAYA ÇALIŞMA 
+        {
+            double x;
+            double y;
+            int deger;
+    
+
+            try
+            {
+                Console.WriteLine("Değer Giriniz:");
+                x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Değer Giriniz:");
+                y = Convert.ToDouble(Console.ReadLine());
+
+                double z = x + y, zz = x - y, zzz = x * y, zzzz = x / y;
+
+                Console.WriteLine("Hangi İşlemi Yapmak İstiyorsunuz?");
+                Console.WriteLine("1-Toplama, 2-Çıkarma, 3-Çarpma, 4-Bölme");
+                deger = Convert.ToInt32(Console.ReadLine());
+
+                if (deger == 1)
+                {
+                    Console.WriteLine(z);
+                }
+                        if (deger == 2)
+                        {
+                            Console.WriteLine(zz);
+                        }
+                                if (deger == 3)
+                                {
+                                    Console.WriteLine(zzz);
+                                }
+                                        if (deger == 4)
+                                        {
+                                            Console.WriteLine(zzzz);
+                                        }
+                                            if (deger >=5)
+                                            {
+                                                Console.WriteLine("Hata");
+                                           }
+                                                 if( deger <= 0)
+                                                        {
+                                                            Console.WriteLine("Hata");
+                                                        }
+
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata " + ex.Message.ToString());
+            }
+      
+
+
+        }
+    }
+}
---
+++ TryCatchDeneme/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("TryCatchDeneme")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TryCatchDeneme")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("2f40e969-5d74-405f-9fef-3d11a9ed1bbe")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
